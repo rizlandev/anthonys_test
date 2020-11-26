d2ed7947a6e6596b51856e9d460b054657ffae8b
rizlan_dev <38829499+rizlandev@users.noreply.github.com>
2020-11-26 11:54:29 +0530

    Create getLineStaticDiscount.php
    added for test

---
+++ getLineStaticDiscount.php
@@ -0,0 +1,33 @@
+{
+  "result": "true",
+  "data": [
+    {
+      "dis_schm_id": 1,
+      "main_cat_id": "1",
+      "appliedItems": [
+        {
+          "p_id": "1"
+        },
+        {
+          "p_id": "2"
+        }
+      ],
+      "discountData": [
+        {
+          "qty_lower": 3,
+          "qty_upper": 4,
+          "dis_rupees":2,
+        },
+        {
+          "qty_lower": 5,
+          "qty_upper": 9,
+          "dis_rupees": 3,
+        }, {
+          "qty_lower": 10,
+          "qty_upper": 11,
+          "dis_rupees": 4,
+        }
+      ]
+    }
+  ]
+}
