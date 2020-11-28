08be94702527db420e72a88825ce31821d318051
rizlan_dev <38829499+rizlandev@users.noreply.github.com>
2020-11-28 15:32:38 +0530

    Create getSlabDiscount.php
    add ed

---
+++ getSlabDiscount.php
@@ -0,0 +1,38 @@
+{
+  "result": true,
+  "data": [
+    {
+      "dis_schm_id": "1",
+      "main_cat_id": "6",
+      "label": "OTHER PACK SLAB DISCOUNT 01",
+      "appliedItems": [
+        {
+          "p_id": "87"
+        },
+        {
+          "p_id": "88"
+        },
+        {
+          "p_id": "89"
+        }
+      ],
+      "discount_range": [
+        {
+          "percentage": "4.00",
+          "lower": "3000.00",
+          "upper": "3999.99"
+        },
+        {
+          "percentage": "5.00",
+          "lower": "4000.00",
+          "upper": "4999.99"
+        },
+        {
+          "percentage": "6.00",
+          "lower": "5000.00",
+          "upper": "99999999.99"
+        }
+      ]
+    }
+  ]
+}
