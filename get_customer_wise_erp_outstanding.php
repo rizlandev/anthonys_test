37cd5accbca86b889645161dd2c3d73a0b2bd9df
rizlan_dev <38829499+rizlandev@users.noreply.github.com>
2020-12-11 10:30:51 +0530

    Create get_customer_wise_erp_outstanding.php
    add outstanding

---
+++ get_customer_wise_erp_outstanding.php
@@ -0,0 +1,13 @@
+{
+  "result": "true",
+  "outstanding_data": [
+    {
+      "outlet_id": "695",
+      "outstanding_amt": "25000"
+    },
+    {
+      "outlet_id": "696",
+      "outstanding_amt": "10000"
+    }
+  ]
+}
