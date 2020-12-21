cd7af42abf07a900dee9e02617de471e9d95d30d
rizlan_dev <38829499+rizlandev@users.noreply.github.com>
2020-12-21 17:59:43 +0530

    Create getUniqueOutletCountDayReport .php
    getUniqueOutletCountDayReport

---
+++ getUniqueOutletCountDayReport .php
@@ -0,0 +1,39 @@
+{
+  "result": true,
+  "data": [
+    {
+      "territory": "HORANA",
+      "timestamp": 1608523544,
+      "unique_outlet_count": [
+        {
+          "cat_id": 1,
+          "cat_name": "Z",
+          "sr_out_count": 42,
+          "today_invo_count": 32,
+          "unique_out_count": 22
+        },
+        {
+          "cat_id": 2,
+          "cat_name": "WT",
+          "sr_out_count": 47,
+          "today_invo_count": 35,
+          "unique_out_count": 20
+        },
+        {
+          "cat_id": 3,
+          "cat_name": "RK",
+          "sr_out_count": 44,
+          "today_invo_count": 36,
+          "unique_out_count": 3
+        },
+        {
+          "cat_id": 5,
+          "cat_name": "HT",
+          "sr_out_count": 46,
+          "today_invo_count": 38,
+          "unique_out_count": 23
+        }
+      ]
+    }
+  ]
+}
