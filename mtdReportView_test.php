0a9ea73f6e1e66a039b27b52f46def26842254be
rizlan_dev <38829499+rizlandev@users.noreply.github.com>
2020-12-21 12:05:07 +0530

    Create mtdReportView_test.php
    test this a dummy

---
+++ mtdReportView_test.php
@@ -0,0 +1,125 @@
+{
+  "result": true,
+  "mtd_target": [
+    {
+      "territory": "HORANA",
+      "timestamp": 1608523544,
+      "month_tar": 3727872.36,
+      "month_day_tar": "2835364",
+      "month_day_arch": 2783986.18,
+      "num_of_day": "16/21",
+      "collage_tar": 1156,
+      "collage_arch_old": 848,
+      "collage_arch": 895,
+      "productivity_tar": 806,
+      "productivity_arch_old": 590,
+      "productivity_arch": 596,
+      "volume_tar_ache": [
+        {
+          "cat_id": 1,
+          "cat_name": "Z",
+          "v_month_tar": "266.20",
+          "v_month_day_tar": "210",
+          "v_month_day_arch": 304.5
+        },
+        {
+          "cat_id": 2,
+          "cat_name": "WT",
+          "v_month_tar": "2215.20",
+          "v_month_day_tar": "1694",
+          "v_month_day_arch": 1557.7
+        },
+        {
+          "cat_id": 3,
+          "cat_name": "RK",
+          "v_month_tar": "1195.58",
+          "v_month_day_tar": "874",
+          "v_month_day_arch": 930.01
+        },
+        {
+          "cat_id": 4,
+          "cat_name": "WW",
+          "v_month_tar": "0.00",
+          "v_month_day_tar": "1",
+          "v_month_day_arch": 0
+        },
+        {
+          "cat_id": 5,
+          "cat_name": "HT",
+          "v_month_tar": 6.48,
+          "v_month_day_tar": "3.48",
+          "v_month_day_arch": 4.7
+        }
+      ],
+      "distribution_tar_ache": [
+        {
+          "cat_id": 1,
+          "cat_name": "Z",
+          "d_month_tar": "260",
+          "d_month_day_tar": "201",
+          "d_month_day_arch": 171
+        },
+        {
+          "cat_id": 2,
+          "cat_name": "WT",
+          "d_month_tar": "680",
+          "d_month_day_tar": "490",
+          "d_month_day_arch": 447
+        },
+        {
+          "cat_id": 3,
+          "cat_name": "RK",
+          "d_month_tar": "500",
+          "d_month_day_tar": "354",
+          "d_month_day_arch": 355
+        },
+        {
+          "cat_id": 4,
+          "cat_name": "WW",
+          "d_month_tar": "1",
+          "d_month_day_tar": "1",
+          "d_month_day_arch": 0
+        },
+        {
+          "cat_id": 5,
+          "cat_name": "HT",
+          "d_month_tar": "80",
+          "d_month_day_tar": "62",
+          "d_month_day_arch": 60
+        }
+      ],
+      "UniqueOutletDistribution_tar_ache": [
+        {
+          "cat_id": 1,
+          "cat_name": "Z",
+          "u_month_tar": "260",
+          "u_month_day_tar": "201",
+          "u_month_day_arch": 171
+        },
+        {
+          "cat_id": 2,
+          "cat_name": "WT",
+          "u_month_tar": "680",
+          "u_month_day_tar": "490",
+          "u_month_day_arch": 447
+        },
+        {
+          "cat_id": 3,
+          "cat_name": "RK",
+          "u_month_tar": "500",
+          "u_month_day_tar": "354",
+          "u_month_day_arch": 355
+        },
+        {
+          "cat_id": 5,
+          "cat_name": "HT",
+          "u_month_tar": "80",
+          "u_month_day_tar": "62",
+          "u_month_day_arch": 60
+        }
+      ],
+      "sku_bill_tar": 0,
+      "sku_bill_arch": "2.4463"
+    }
+  ]
+}
