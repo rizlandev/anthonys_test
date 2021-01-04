3596036ece9f3f9b3f281ca92c66a49623c6e777
rizlan_dev <38829499+rizlandev@users.noreply.github.com>
2021-01-04 10:15:53 +0530

    Create salesReturnForItemBatches_hotel.php
    salesReturnForItemBatches_hotel

---
+++ salesReturnForItemBatches_hotel.php
@@ -0,0 +1,23 @@
+{
+  "result": "true",
+  "repid": "660",
+  "product": [
+    {
+      "itemId": "1",
+      "batches": [
+        {
+          "batch_no": "3016",
+          "price": "590.00"
+        },
+        {
+          "batch_no": "2971",
+          "price": "630.00"
+        },
+        {
+          "batch_no": "2931",
+          "price": "670.00"
+        }
+      ]
+    }
+  ]
+} 
