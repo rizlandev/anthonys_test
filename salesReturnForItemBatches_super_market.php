ba35158679c4651c05111fd210d211a8fc8ba6c1
rizlan_dev <38829499+rizlandev@users.noreply.github.com>
2021-01-04 10:17:48 +0530

    Create salesReturnForItemBatches_super_market.php
    salesReturnForItemBatches_super_market

---
+++ salesReturnForItemBatches_super_market.php
@@ -0,0 +1,36 @@
+{
+  "result": "true",
+  "repid": "660",
+  "SM_Prices": [
+    {
+      "oc_name": "S/M A",
+      "out_main_cat_id": "6",
+      "sub_cats": [
+        {
+          "out_sub_cat_id": "6",
+          "out_sub_cat_name": "Family Super",
+          "price": [
+            {
+              "iditem": "1",
+              "sd_id": "1097",
+              "batches": [
+                {
+                  "batch_no": "3016",
+                  "price": "590.00"
+                },
+                {
+                  "batch_no": "2971",
+                  "price": "630.00"
+                },
+                {
+                  "batch_no": "2931",
+                  "price": "670.00"
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    }
+  ]
+}
