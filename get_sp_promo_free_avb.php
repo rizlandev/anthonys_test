7db5e1808c30798072ae2226216fbca53a50b6ca
rizlan_dev <38829499+rizlandev@users.noreply.github.com>
2021-01-07 09:49:52 +0530

    Create get_sp_promo_free_avb.php
    get_sp_promo_free_avb.php

---
+++ get_sp_promo_free_avb.php
@@ -0,0 +1,26 @@
+{
+  "result": "true",
+  "outlet_free": {
+    "outlet": [
+      {
+        "freeIssues": [
+          [
+            {
+              "promo_free_structure_id": "12"
+            }
+          ]
+        ],
+        "out_id": "72841"
+      }, {
+        "freeIssues": [
+          [
+            {
+              "promo_free_structure_id": "10"
+            }
+          ]
+        ],
+        "out_id": "73734"
+      }
+    ]
+  }
+}
