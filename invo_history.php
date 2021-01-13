c1e59934a2a0813fd74a562a65e3308fe2f1dd82
rizlan_dev <38829499+rizlandev@users.noreply.github.com>
2021-01-13 12:47:51 +0530

    Create invo_history.php
    invo_history.php

---
+++ invo_history.php
@@ -0,0 +1,190 @@
+{
+    "result": "true",
+    "invoice": [
+        {
+            "s_id": "6229",
+            "invoice_id": "INV/DL/134/000123",
+            "invo_sp_line_discount": "0.00",
+            "outlet_Name": "TEST OUTLET",
+            "wtcl_out_id": "1",
+            "outlet_address": "TEST",
+            "outlet_mobile": "011",
+            "invo_date": "2021-01-13",
+            "invo_time": "12:18:42",
+            "invo_line_discount": "15.00",
+            "invo_assorted_discount": "20.00",
+            "invo_range_discount": "437.38",
+            "invo_raffle_no": "",
+            "invo_pay_method": "CASH ",
+            "due_date": "2021-01-13",
+            "order_item": [
+                {
+                    "o_item_id": "1",
+                    "o_item_description": "M- Coconut Bag (12)",
+                    "o_item_sequence": "1",
+                    "o_itm_price": "245.00",
+                    "o_itm_value": "3185.00",
+                    "o_item_units": "13" 
+                },
+                {
+                    "o_item_id": "2",
+                    "o_item_description": "Peanut Bag (12)",
+                    "o_item_sequence": "2",
+                    "o_itm_price": "245.00",
+                    "o_itm_value": "735.00",
+                    "o_item_units": "3"
+                },
+                {
+                    "o_item_id": "3",
+                    "o_item_description": "M- Sherbet Bag (12)",
+                    "o_item_sequence": "3",
+                    "o_itm_price": "245.00",
+                    "o_itm_value": "980.00",
+                    "o_item_units": "4"
+                },
+                {
+                    "o_item_id": "4",
+                    "o_item_description": "Tamarind Bag (12)",
+                    "o_item_sequence": "4",
+                    "o_itm_price": "245.00",
+                    "o_itm_value": "735.00",
+                    "o_item_units": "3"
+                },
+                {
+                    "o_item_id": "5",
+                    "o_item_description": "C- Mint Bag (12)",
+                    "o_item_sequence": "5",
+                    "o_itm_price": "245.00",
+                    "o_itm_value": "980.00",
+                    "o_item_units": "4"
+                },
+                {
+                    "o_item_id": "17",
+                    "o_item_description": "L- Pop Strawberry Bottle (6)",
+                    "o_item_sequence": "17",
+                    "o_itm_price": "610.00",
+                    "o_itm_value": "1220.00",
+                    "o_item_units": "2"
+                },
+                {
+                    "o_item_id": "18",
+                    "o_item_description": "B -Gum Box (40)",
+                    "o_item_sequence": "18",
+                    "o_itm_price": "140.00",
+                    "o_itm_value": "3500.00",
+                    "o_item_units": "25"
+                },
+                {
+                    "o_item_id": "45",
+                    "o_item_description": "L- Pop Ass Bottle (6)-Milady",
+                    "o_item_sequence": "45",
+                    "o_itm_price": "390.00",
+                    "o_itm_value": "1170.00",
+                    "o_item_units": "3"
+                },
+                {
+                    "o_item_id": "76",
+                    "o_item_description": "B- wafers (V) 400g Pkt (3)",
+                    "o_item_sequence": "76",
+                    "o_itm_price": "203.00",
+                    "o_itm_value": "812.00",
+                    "o_item_units": "4"
+                },
+                {
+                    "o_item_id": "77",
+                    "o_item_description": "B- wafers (C) 400g Pkt (3)",
+                    "o_item_sequence": "77",
+                    "o_itm_price": "203.00",
+                    "o_itm_value": "812.00",
+                    "o_item_units": "4"
+                },
+                {
+                    "o_item_id": "78",
+                    "o_item_description": "B- wafers (S) 400g Pkt (3)",
+                    "o_item_sequence": "78",
+                    "o_itm_price": "203.00",
+                    "o_itm_value": "812.00",
+                    "o_item_units": "4"
+                },
+                {
+                    "o_item_id": "87",
+                    "o_item_description": "B C- Cream 100g Pkt (30)",
+                    "o_item_sequence": "87",
+                    "o_itm_price": "57.25",
+                    "o_itm_value": "858.75",
+                    "o_item_units": "15"
+                },
+                {
+                    "o_item_id": "88",
+                    "o_item_description": "B- Asso- Giftt - Pkt (12)",
+                    "o_item_sequence": "88",
+                    "o_itm_price": "220.00",
+                    "o_itm_value": "3520.00",
+                    "o_item_units": "16"
+                },
+                {
+                    "o_item_id": "89",
+                    "o_item_description": "B Tea Time Asso Gift (20)",
+                    "o_item_sequence": "89",
+                    "o_itm_price": "122.50",
+                    "o_itm_value": "1592.50",
+                    "o_item_units": "13"
+                },
+                {
+                    "o_item_id": "90",
+                    "o_item_description": "B- wafers Asso Gift 340gPkt(12)",
+                    "o_item_sequence": "90",
+                    "o_itm_price": "220.00",
+                    "o_itm_value": "1320.00",
+                    "o_item_units": "6"
+                }
+            ],
+            "free_item": [
+                {
+                    "f_item_id": "1",
+                    "f_item_description": "M- Coconut Bag (12)",
+                    "f_item_sequence": "1",
+                    "f_itm_price": "245.00",  
+                    "f_itm_value": "245.00",
+                    "f_item_units": "1"    
+                },
+                {
+                    "f_item_id": "5",
+                    "f_item_description": "C- Mint Bag (12)",
+                    "f_item_sequence": "5",
+                    "f_itm_price": "245.00",
+                    "f_itm_value": "245.00",
+                    "f_item_units": "1"
+                },
+                {
+                    "f_item_id": "76",
+                    "f_item_description": "B- wafers (V) 400g Pkt (3)",
+                    "f_item_sequence": "76",
+                    "f_itm_price": "245.00",
+                    "f_itm_value": "245.00",
+                    "f_item_units": "1"
+                }
+            ],
+            "sales_ret_item": [
+                {
+                    "sr_item_id": "1",
+                    "sr_item_description": "M- Coconut Bag (12)",
+                    "sr_item_sequence": "1",
+                    "sr_itm_price": "245.00",
+                    "sr_itm_value": "245.00",
+                    "sr_item_units": "1"  
+                }
+            ],
+            "market_ret_item": [
+                {
+                    "mr_item_id": "3",
+                    "mr_item_description": "M- Sherbet Bag (12)",
+                    "mr_item_sequence": "3",
+                    "mr_itm_price": "25.00",
+                    "mr_itm_value": "75.00",
+                    "mr_item_units": "3" 
+                }
+            ]
+        }
+    ]
+}
