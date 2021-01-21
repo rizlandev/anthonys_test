fad452f2187ab4d8a64602bf7d38bd737df3909f
rizlan_dev <38829499+rizlandev@users.noreply.github.com>
2021-01-21 15:36:57 +0530

    Create getProduct_trade.php

---
+++ getProduct_trade.php
@@ -0,0 +1,67 @@
+{
+  "result": "true",
+  "max_timestamp": "1610634959000",
+  "actual_pro_count": 5,
+  "product": [
+    {
+      "product_id": 3,
+      "product_code": "072",
+      "product_short_name": "HOTEL CHICKEN K - 5 (1.400-1.600)",
+      "product_name": "HOTEL CHICKEN K - 5 (1.400-1.600)",
+      "product_cat_id": 3,
+      "product_sub_cat_id": 2,
+      "product_billing_type": 0,
+      "unit_weight": "0.00",
+      "product_pack_size": 0,
+      "product_kilo_grams": 0
+    },
+    {
+      "product_id": 121,
+      "product_code": "700",
+      "product_short_name": "DRUMSTIC POPCORN SPECIAL PACK",
+      "product_name": "DRUMSTIC POPCORN SPECIAL PACK",
+      "product_cat_id": 4,
+      "product_sub_cat_id": 1,
+      "product_billing_type": 1,
+      "unit_weight": "1.60",
+      "product_pack_size": 1,
+      "product_kilo_grams": "1.00"
+    },
+    {
+      "product_id": 124,
+      "product_code": "065",
+      "product_short_name": "CHICKEN CUBES 1 KG",
+      "product_name": "CHICKEN CUBES 1 KG",
+      "product_cat_id": 4,
+      "product_sub_cat_id": 2,
+      "product_billing_type": 2,
+      "unit_weight": "1.00",
+      "product_pack_size": 0,
+      "product_kilo_grams": 0
+    },
+    {
+      "product_id": 4,
+      "product_code": "069",
+      "product_short_name": "HOTEL CHICKEN K - 2 (1.200-1.300)",
+      "product_name": "HOTEL CHICKEN K - 2 (1.200-1.300)",
+      "product_cat_id": 1,
+      "product_sub_cat_id": 2,
+      "product_billing_type": 0,
+      "unit_weight": "0.00",
+      "product_pack_size": 0,
+      "product_kilo_grams": 0
+    },
+    {
+      "product_id": 49,
+      "product_code": "003",
+      "product_short_name": "FULL CHICKEN - CHILL",
+      "product_name": "FULL CHICKEN - CHILL",
+      "product_cat_id": 2,
+      "product_sub_cat_id": 4,
+      "product_billing_type": 0,
+      "unit_weight": "0.00",
+      "product_pack_size": 0,
+      "product_kilo_grams": 0
+    }
+  ]
+}
