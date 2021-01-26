40d5578a20fe367cc4fe2157f62be341e6b6d35c
rizlan_dev <38829499+rizlandev@users.noreply.github.com>
2021-01-26 16:36:15 +0530

    Create get_outlet_wise_product_visibility.php
    xzxbzm

---
+++ get_outlet_wise_product_visibility.php
@@ -0,0 +1,23 @@
+{
+  "result": "true",
+  "outlet": [
+    {
+      "blocked_items": [
+        {
+          "item_id": "285"
+        },
+        {
+          "item_id": "435"
+        }
+      ],
+      "out_id": "159888"
+    },  {
+      "blocked_items": [
+        {
+          "item_id": "285"
+        }
+      ],
+      "out_id": "159887"
+    }
+  ]
+}
