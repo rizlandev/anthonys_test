da7bcc6d3c2a2d490093f20db998d170cf46e5ec
rizlan_dev <38829499+rizlandev@users.noreply.github.com>
2021-01-29 16:54:18 +0530

    Update get_outlet_wise_product_visibility.php
    app edited

--- get_outlet_wise_product_visibility.php
+++ get_outlet_wise_product_visibility.php
@@ -3,20 +3,24 @@
   "outlet": [
     {
       "blocked_items": [
-        {
-          "item_id": "285"
-        },
-        {
-          "item_id": "435"
-        }
+        [
+          {
+            "item_id": "285"
+          },
+          {
+            "item_id": "435"
+          }
+        ]
       ],
       "out_id": "159888"
     },
     {
       "blocked_items": [
-        {
-          "item_id": "285"
-        }
+        [
+          {
+            "item_id": "285"
+          }
+        ]
       ],
       "out_id": "159887"
     }
