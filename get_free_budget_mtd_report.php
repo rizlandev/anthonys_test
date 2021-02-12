d006d1e8e30172cc574a29d45107c0f136026b17
rizlan_dev <38829499+rizlandev@users.noreply.github.com>
2021-02-12 13:02:08 +0530

    Create get_free_budget_mtd_report.php

---
+++ get_free_budget_mtd_report.php
@@ -0,0 +1,45 @@
+{
+  "result": "true",
+  "user_id": 134,
+  "server_time": 123445533,
+  "free_budget": [
+    {
+      "budget_items": [
+        [
+          {
+            "item_id": "396",
+            "item_qty": 10
+          },
+          {
+            "item_id": "397",
+            "item_qty": 12
+          }
+        ]
+      ],
+      "budget_id": "1",
+      "budget_label": "Z-50g , Z-100g BUDGET PROPOSAL-1",
+      "budget_period": "2021/02/01 - 2021/02/28",
+      "budget_allocated": 10000,
+      "budget_balance": 5000
+    },
+    {
+      "budget_items": [
+        [
+          {
+            "item_id": "384",
+            "item_qty": 15
+          },
+          {
+            "item_id": "387",
+            "item_qty": 20
+          }
+        ]
+      ],
+      "budget_id": "2",
+      "budget_label": "WT-50g , WT-100g BUDGET PROPOSAL-2",
+      "budget_period": "2021/02/01 - 2021/02/28",
+      "budget_allocated": 15000,
+      "budget_balance": 10000
+    }
+  ]
+}
