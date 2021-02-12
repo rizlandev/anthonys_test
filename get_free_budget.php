0f190c7ee4d592f62a1fabcf3d1b3931e5cb5d15
rizlan_dev <38829499+rizlandev@users.noreply.github.com>
2021-02-12 11:46:21 +0530

    Create get_free_budget.php
    budget data

---
+++ get_free_budget.php
@@ -0,0 +1,38 @@
+{
+  "result": "true",
+  "user_id": 134,
+  "free_budget": [
+    {
+      "budget_items": [
+        [
+          {
+            "item_id": "396"
+          },
+          {
+            "item_id": "397"
+          }
+        ]
+      ],
+      "budget_id": "1",
+      "budget_label": "Z-50g , Z-100g BUDGET PROPOSAL-1",
+      "budget_allocated": 10000,
+      "budget_balance": 5000
+    },
+    {
+      "budget_items": [
+        [
+          {
+            "item_id": "384"
+          },
+          {
+            "item_id": "387"
+          }
+        ]
+      ],
+      "budget_id": "2",
+      "budget_label": "WT-50g , WT-100g BUDGET PROPOSAL-2",
+      "budget_allocated": 15000,
+      "budget_balance": 10000
+    }
+  ]
+}
