a0972869755a48108023a6f972d791aed6a56b09
rizlan_dev <38829499+rizlandev@users.noreply.github.com>
2021-03-08 13:07:55 +0530

    Create get_shop_cat_with_focus_pro.php
    added focus product

---
+++ get_shop_cat_with_focus_pro.php
@@ -0,0 +1,72 @@
+{
+  "result": "true",
+  "shop_category": [
+    {
+      "sh_cat_id": "5",
+      "sh_cat_name": "WS",
+      "allow_creation": "0",
+      "allow_deletion": "1",
+      "focus_items": []
+    },
+    {
+      "sh_cat_id": "7",
+      "sh_cat_name": "A",
+      "allow_creation": "0",
+      "allow_deletion": "1",
+      "focus_items": []
+    },
+    {
+      "sh_cat_id": "8",
+      "sh_cat_name": "B",
+      "allow_creation": "0",
+      "allow_deletion": "1",
+      "focus_items": [
+        {
+          "item_id": "406"
+        },
+        {
+          "item_id": "170"
+        }
+      ]
+    },
+    {
+      "sh_cat_id": "9",
+      "sh_cat_name": "C",
+      "allow_creation": "0",
+      "allow_deletion": "1",
+      "focus_items": [
+        {
+          "item_id": "384"
+        }
+      ]
+    },
+    {
+      "sh_cat_id": "10",
+      "sh_cat_name": "D",
+      "allow_creation": "0",
+      "allow_deletion": "1",
+      "focus_items": []
+    },
+    {
+      "sh_cat_id": "11",
+      "sh_cat_name": "E",
+      "allow_creation": "0",
+      "allow_deletion": "1",
+      "focus_items": []
+    },
+    {
+      "sh_cat_id": "12",
+      "sh_cat_name": "SMMT",
+      "allow_creation": "0",
+      "allow_deletion": "1",
+      "focus_items": []
+    },
+    {
+      "sh_cat_id": "13",
+      "sh_cat_name": "WS+SMMT",
+      "allow_creation": "0",
+      "allow_deletion": "1",
+      "focus_items": []
+    }
+  ]
+}
