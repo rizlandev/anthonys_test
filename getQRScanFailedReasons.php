b5c7985e097b497f0d571b29b831660cf5fcdab7
rizlan_dev <38829499+rizlandev@users.noreply.github.com>
2021-04-16 15:18:40 +0530

    Create getQRScanFailedReasons.php
    added

---
+++ getQRScanFailedReasons.php
@@ -0,0 +1,17 @@
+{
+    "result": "true",
+    "reason": [
+        {
+            "reason_id": "100",
+            "reason_name": "QR CODE NOT FOUND"
+        },
+        {
+            "reason_id": "101",
+            "reason_name": "SHOP CLOSED"
+        },
+        {
+            "reason_id": "102",
+            "reason_name": "NEW CUSTOMER"
+        }
+    ]
+}
